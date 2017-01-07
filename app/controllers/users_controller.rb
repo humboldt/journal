e0963f2aec5640c3ce45d3d729c9876ae9d37099
João Vitor Flores <vitor@mailbox.org>
2017-01-07 14:56:05 -0300

    Finished user edit, update and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       redirect_to @user
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to root_url
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def new
@@ -17,10 +30,35 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root   'static_pages#home'
-  get    '/signup', to: 'users#new'
-  post   '/signup', to: 'users#create'
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get    '/signup',         to: 'users#new'
+  post   '/signup',         to: 'users#create'
+  get    '/login',          to: 'sessions#new'
+  post   '/login',          to: 'sessions#create'
+  delete '/logout',         to: 'sessions#destroy'
   resources :users
 end
